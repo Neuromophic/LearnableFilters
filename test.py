864af05e7f46c179b1c35d5b3c8d10659ef44a5c
px3192@kit.edu <px3192@uc2n995.localdomain>
2023-07-13 17:37:57 +0200

    update

---
+++ test.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#SBATCH --job-name=LPF
+#SBATCH --error=%x.%j.err
+#SBATCH --output=%x.%j.out
+#SBATCH --mail-user=hzhao@teco.edu
+#SBATCH --export=ALL
+#SBATCH --partition=dev_single
+#SBATCH --cpus-per-task=1
+
+import os
+import sys
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), 'utils'))
+from configuration import *
+import torch
+import pprint
+from utils import *
+import PrintedLearnableFilter as pNN
+
+args = parser.parse_args()
+
+for seed in range(10):
+
+    args.SEED = seed
+    args = FormulateArgs(args)
+        
+    print(f'Training network on device: {args.DEVICE}.')
+    MakeFolder(args)
+
+    train_loader, datainfo = GetDataLoader(args, 'train')
+    valid_loader, datainfo = GetDataLoader(args, 'valid')
+    test_loader, datainfo = GetDataLoader(args, 'test')
+    pprint.pprint(datainfo)
+
+    SetSeed(args.SEED)
+
+    setup = f"pLF_data_{args.DATASET:02d}_{datainfo['dataname']}_seed_{args.SEED:02d}.model"
+    print(f'Training setup: {setup}.')
+
+    msglogger = GetMessageLogger(args, setup)
+    msglogger.info(f'Training network on device: {args.DEVICE}.')
+    msglogger.info(f'Training setup: {setup}.')
+    msglogger.info(datainfo)
+
+    if os.path.isfile(f'{args.savepath}/{setup}'):
+        print(f'{setup} exists, skip this training.')
+        msglogger.info('Training was already finished.')
+    else:
+        pnn = pNN.PrintedNeuralNetwork(args, datainfo['N_feature'], datainfo['N_class'], 2).to(args.DEVICE)
+
+        msglogger.info(f'Number of parameters that are learned in this experiment: {len(pnn.GetParam())}.')
+
+        lossfunction = pNN.LFLoss(args).to(args.DEVICE)
+        optimizer = torch.optim.Adam(pnn.GetParam(), lr=args.LR)
+
+        if args.PROGRESSIVE:
+            pnn, best = train_pnn_progressive(pnn, train_loader, valid_loader, lossfunction, optimizer, args, msglogger, UUID=setup)
+        else:
+            pnn, best = train_pnn(pnn, train_loader, valid_loader, lossfunction, optimizer, args, msglogger, UUID=setup)
+
+        if best:
+            if not os.path.exists(f'{args.savepath}/'):
+                os.makedirs(f'{args.savepath}/')
+            torch.save(pnn, f'{args.savepath}/{setup}')
+            msglogger.info('Training if finished.')
+        else:
+            msglogger.warning('Time out, further training is necessary.')
